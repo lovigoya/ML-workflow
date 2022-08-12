5220fa27e040800509134a58b59f7bfc3cdfbd7d
Lovi Goyal <amitgarg@Amits-Air-2.hsd1.ca.comcast.net>
2022-08-12 15:41:58 -0700

    build ML pipeline

---
+++ script.py
@@ -0,0 +1,26 @@
+def get_data():
+    print("donothing")
+
+def build_model():
+    print("do nothing")
+
+def preprocess_data():
+    print("do nothing")
+
+
+def create_pipeline():
+    get_data()
+    preprocess_data()
+    build_model()
+    pipeline_obj = 5
+    return pipeline_obj
+
+def inference():
+    pipeline = create_pipeline()
+    # pipeline.predict()
+    return 1
+
+
+if __name__=="__main__":
+    inference()
+
